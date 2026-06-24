7a82111ff290d07e33cd0f1ca91649b98dfcac7e
blyuu <junsoo.k@outlook.com>
2026-06-24 23:58:44 +0900

    solved

--- BOJ/15649/sol.cpp
+++ BOJ/15649/sol.cpp
@@ -3,15 +3,51 @@
 
 using namespace std;
 
-void dfs()
+void dfs(vector<bool>& visited, vector<int>& path, int N, int M, vector<vector<int>>& result)
 {
+    if(path.size() == M)
+    {
+        result.push_back(path);
+        return;
+    }
+
+    for(int i = 1; i <= N; i++)
+    {
+        if(!visited[i])
+        {
+            path.push_back(i);
+            visited[i] = true;
+            dfs(visited,path,N,M,result);
+            path.pop_back();    
+            visited[i] = false;
+        }
+        
+    }
 
 }
+
 
 int main()
 {
     int N, M;
     cin >> N >> M;
 
+    vector<int> path;
+    vector<bool> visited(N+1, false);
+    vector<vector<int>> result;
+
+    dfs(visited,path, N, M, result);
+
+    for(auto c: result)
+    {
+        for (auto d : c)
+        {
+            cout << d;
+        }
+
+        cout << endl;
+    }
+
     return 0;
+
 }
